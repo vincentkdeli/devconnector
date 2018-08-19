f966caa5722b74eaf529b90568bc2313fbc03ae9
Vincent <vincentkdeli@gmail.com>
2018-08-19 18:55:50 +0700

    Securing keys for deployment pt.2

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://vincent:vincent8@ds151059.mlab.com:51059/dconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
